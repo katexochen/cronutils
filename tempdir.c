c39df37c6c280e3f73ea57cfa598b8447e5a58fe
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-01-31 21:23:44 +0100

    fix function declaration without a prototype
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- tempdir.c
+++ tempdir.c
@@ -16,6 +16,8 @@ limitations under the License.
 
 #define _GNU_SOURCE /* asprintf */
 
+#include "tempdir.h"
+
 #include <errno.h>
 #include <limits.h>
 #include <pwd.h>
@@ -28,49 +30,48 @@ limitations under the License.
 #include <syslog.h>
 #include <unistd.h>
 
-#include "tempdir.h"
+const char* template = "/tmp/cronutils-";
+char* dirname = NULL;
 
-const char * template = "/tmp/cronutils-";
-char * dirname = NULL;
+char* make_tempdir(void) {
+    uid_t uid;
+    struct passwd* pw;
+    struct stat st;
 
-char * make_tempdir() {
-  uid_t uid;
-  struct passwd * pw;
-  struct stat st;
-
-  uid = geteuid();
-  if ((pw = getpwuid(uid)) == NULL) {
-    perror("getpwuid");
-    exit(EX_OSERR);
-  }
-  if (asprintf(&dirname, "%s%s", template, pw->pw_name) == -1) {
-    perror("asprintf");
-    exit(EX_OSERR);
-  }
-  syslog(LOG_DEBUG, "temp dir is %s\n", dirname);
-  if (mkdir(dirname, S_IRWXU) < 0) {
-    if (errno == EEXIST) {
-      if (stat(dirname, &st) != 0) {
-        perror("stat");
+    uid = geteuid();
+    if ((pw = getpwuid(uid)) == NULL) {
+        perror("getpwuid");
         exit(EX_OSERR);
-      }
-      if (!S_ISDIR(st.st_mode)) {
-        syslog(LOG_ERR, "%s is not a directory\n", dirname);
-        exit(EX_IOERR);
-      }
-      if (st.st_uid != uid) {
-        syslog(LOG_ERR, "%s is not owned by %s\n", dirname, pw->pw_name);
-        exit(EXIT_FAILURE);
-      }
-      if (!(st.st_mode & S_IRWXU)) {
-        syslog(LOG_ERR, "%s has insecure permissions %u\n",
-               dirname, st.st_mode);
-        exit(EXIT_FAILURE);
-      }
-    } else {
-      perror("mkdir");
-      exit(EX_OSERR);
     }
-  }
-  return dirname;
+    if (asprintf(&dirname, "%s%s", template, pw->pw_name) == -1) {
+        perror("asprintf");
+        exit(EX_OSERR);
+    }
+    syslog(LOG_DEBUG, "temp dir is %s\n", dirname);
+    if (mkdir(dirname, S_IRWXU) < 0) {
+        if (errno == EEXIST) {
+            if (stat(dirname, &st) != 0) {
+                perror("stat");
+                exit(EX_OSERR);
+            }
+            if (!S_ISDIR(st.st_mode)) {
+                syslog(LOG_ERR, "%s is not a directory\n", dirname);
+                exit(EX_IOERR);
+            }
+            if (st.st_uid != uid) {
+                syslog(LOG_ERR, "%s is not owned by %s\n", dirname,
+                       pw->pw_name);
+                exit(EXIT_FAILURE);
+            }
+            if (!(st.st_mode & S_IRWXU)) {
+                syslog(LOG_ERR, "%s has insecure permissions %u\n", dirname,
+                       st.st_mode);
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            perror("mkdir");
+            exit(EX_OSERR);
+        }
+    }
+    return dirname;
 }
